ebad166a1cd0ebe5a5c9e672aa80756ac79a56dd
Felix <felix@becker.im>
2025-03-12 23:22:50 -0300

    more humble

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
           Hi ✽ I&apos;m Felix{" "}
         </h2>
         <div className="text-xs md:text-xl max-w-3xl text-center px-4 sm:px-8 md:px-12">
-          Currently working on automating eCommerce operations with LLMs. <br /> <br /> Before, I worked as an Associate at Heartcore Capital, where I focused on deep tech areas (think: dev tooling, infrastructure, and robotics) — shoutout to Quesma (Google Translate for databases) and FlexAI (Nvidia CUDA for Intel and AMD chips). <br /> <br /> Before that, I worked with the founders of Flaschenpost on launching the e-grocery business. <br /> <br /> I learned coding (PHP, JS, and Ruby) in elementary school, and after taking a detour in Finance, I am back to improving my skills on the weekends.
+          Currently: Vibe coding and automating mundane tasks in eCommerce. <br /> <br /> Before: Associate at Heartcore Capital, where I focused on deep tech areas (think: dev tooling, infrastructure, and robotics) - eg. <a href="https://quesma.com/" target="_blank" rel="noopener noreferrer" className="pointer-events-auto">Quesma</a> (Google Translate for databases) and <a href="https://www.flex.ai/" target="_blank" rel="noopener noreferrer" className="pointer-events-auto">FlexAI</a>. <br /> <br /> Before that, I worked with the founders of Flaschenpost on launching the e-grocery business. <br />
         </div>
       </div>
 
