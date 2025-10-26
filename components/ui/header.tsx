c673096bfaf631a959c6b12d814c8f0b6edc5ed0
Felix <felix@becker.im>
2025-10-26 21:40:25 +0100

    headline

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -29,7 +29,7 @@ const Header: React.FC = () => {
           Hi ✽ I&apos;m Felix{" "}
         </h2>
         <div className="text-xs md:text-xl max-w-3xl text-center px-4 sm:px-8 md:px-12">
-          Currently: Vibe coding and automating mundane tasks in eCommerce. <br /> <br /> Before: Associate at Heartcore Capital, where I focused on deep tech areas (think: dev tooling, infrastructure, and robotics) - eg. <a href="https://quesma.com/" target="_blank" rel="noopener noreferrer" className="pointer-events-auto">Quesma</a> (Google Translate for databases) and <a href="https://www.flex.ai/" target="_blank" rel="noopener noreferrer" className="pointer-events-auto">FlexAI</a>. <br />
+          Currently: Vibe coding and scaling an eCommerce company. <br /> <br /> Before: Associate at Heartcore Capital, where I focused on deep tech areas (think: dev tooling, infrastructure, and robotics) - eg. <a href="https://quesma.com/" target="_blank" rel="noopener noreferrer" className="pointer-events-auto">Quesma</a> (Google Translate for databases) and <a href="https://www.flex.ai/" target="_blank" rel="noopener noreferrer" className="pointer-events-auto">FlexAI</a>. <br />
         </div>
       </div>
 
