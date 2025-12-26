810874da86dfe5bfc1430e219a6a9fe3c417edfa
Felix Becker <felix@becker.im>
2025-12-26 13:15:45 +0100

    Deactivate walking stick man (DelayedScrollBuddy)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import DelayedScrollBuddy from "@/components/delayed-scroll-buddy";
 import Script from "next/script";
 
 const geistSans = Geist({
@@ -75,7 +74,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <DelayedScrollBuddy />
       </body>
     </html>
   );
